45a5a6b7bb1accebf5923929b553d515f3f846eb
v-i-k-a-s <vikas.11081997@gmail.com>
2023-06-11 15:48:42 +0530

    changes-state

--- src/Movies.jsx
+++ src/Movies.jsx
@@ -1,11 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
 import Movie from "./Movie";
 const Movies = () => {
-  const [movieData, setMovieData] = useState({ movieName: "batman", imdb: 10 });
-  return <Movie movieInfo={movieData} />;
+  let movieData = [
+    {
+      movieName: "batman",
+      imdb: 8.2,
+    },
+    {
+      movieName: "superman",
+      imdb: 8.2,
+    },
+    {
+      movieName: "superman-2",
+      imdb: 8.2,
+    },
+  ];
+  return <Movie movieInfo={movieData[0]} />;
   //return React.createElement("div", {}, MovieReactElements);
 };
 export default Movies;
 //Componenet --> function /class
 //compnentDidUpdate
-//useEffect(()=>{},[])
+//useEffect(()=>{},)
